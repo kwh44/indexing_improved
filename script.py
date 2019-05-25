9e447949306f877a04512929d43837664e707d90
franchukpetro <franchuk.p@ucu.edu.ua>
2019-05-25 19:49:30 +0300

    script added

--- script.py
+++ script.py
@@ -1,53 +1,62 @@
 #!/usr/bin/python3.7
 
-import glob
 import os
-import subprocess
 import sys
+import subprocess
 
-if __name__ == '__main__':
-    # display help info
-    if (len(sys.argv) == 2 and sys.argv[1] in ("--help", "-help")):
-        print("Usage: ./script configuration_file_path number_of_iterations")
-        exit(0)
-    # check number of arguments
-    if len(sys.argv) != 3:
-        raise TypeError('Invalid number of arguments provided.\nPlease specify configuration file and number of runs.')
-    number_of_runs = int(sys.argv[2])
-    configuration_file = sys.argv[1]
-    executable = './**/parallel_indexing.exe' if os.name == 'nt' else './**/parallel_indexing'
+prog = "indexing"
+config = "config.dat"  # default path to config file
+root_path = None
 
-    with open(configuration_file, "r") as file:
-        config = file.read().split("\n")
-        output_alphabet, output_count = config[1], config[2]
+if os.name == "nt":
+    prog = prog + ".exe."
 
-    output = subprocess.run([glob.glob(executable)[0], configuration_file], stdout=subprocess.PIPE).stdout.decode(
-        'utf-8')
-    with open(output_alphabet, "r") as file:
-        results = file.read()
+for root, directories, file_list in os.walk('.'):
+    if prog in file_list:
+        if os.name == "nt":
+            prog = root + '\\' + prog
+            root_path = root + '\\'
+        else:
+            prog = root + '/' + prog
+            root_path = root + '/'
+        break
 
-    message = ""
+if len(sys.argv) < 2:
+    print("Example of using: script.py <repetitions_number> <config>")
+    exit(0)
 
-    # run methods some amount of times
-    res = [[] for i in range(3)]
-    for j in range(len(res)):
-        for i in range(number_of_runs):
-            output = subprocess.run([glob.glob(executable)[0], configuration_file],
-                                    stdout=subprocess.PIPE).stdout.decode('utf-8')
+if len(sys.argv) == 3:
+    config = ".\\" + sys.argv[2]
 
-            # get time of method by parsing output of program
-            res[0].append(float(output.split("Total time: ")[1].split()[0]))
-            res[1].append(float(output.split("Reading time: ")[1].split()[0]))
-            res[2].append(float(output.split("Counting time: ")[1].split()[0]))
+if name == "main":
+    num = int(sys.argv[1])
+    parallel_results = []
 
-            with open(output_alphabet, "r") as file:
-                local_results = file.read()
+    with open(config, "r") as conf_file:
+        conf_file.readline()
+        res_a = conf_file.readline().split('"')[-2]
+        res_n = conf_file.readline().split('"')[-2]
 
-            if local_results != results:
-                message = "The results are not the same\n"
+    parallel_res = subprocess.run([prog, config], shell=False, stdout=subprocess.PIPE)
 
-    # print results and comparisons
-    print("Total time:\t", min(res[0]))
-    print("Reading time:\t", min(res[1]))
-    print("Counting time:\t", min(res[2]))
-    print(message, end="")
+    with open(res_a, "r") as par_file_a:
+        parallel_res_a = par_file_a.read()
+    with open(res_n, "r") as par_file_n:
+        parallel_res_n = par_file_n.read()
+
+    parallel_results.append(float(parallel_res.stdout.decode().strip().split()[-1]))
+
+    for _ in range(num - 1):
+        parallel_results.append(
+            float(subprocess.run([prog, config], shell=False, stdout=subprocess.PIPE).
+                  stdout.decode().strip().split()[-1]))
+
+        with open(res_a, "r") as par_file_a1:
+            parallel_res_a1 = par_file_a1.read()
+        with open(res_n, "r") as par_file_n1:
+            parallel_res_n1 = par_file_n1.read()
+
+        if (parallel_res_a != parallel_res_a1) or (parallel_res_n != parallel_res_n1):
+            raise ValueError("Results doesn`t match!")
+
+    print("Best time is {0}.".format(min(parallel_results)))
